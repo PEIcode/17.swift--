b18d32e3340131311563b0ad0c4a3172ae413bfd
Pei <1367890978@qq.com>
2018-08-03 10:04:01 +0800

    Initial Commit

---
+++ 17.swift-高级函数/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  17.swift-高级函数
+//
+//  Created by xingshulin on 2018/8/3.
+//  Copyright © 2018年 xingshulin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
